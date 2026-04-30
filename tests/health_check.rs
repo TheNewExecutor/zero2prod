ef8ba21e31fff668c71295b1064a4418446cce22
Jonathan Sarker <thenewexecutor@gmail.com>
2026-04-29 17:32:37 -0700

    Updated tests and dependency versions
    Tests were modified to use sqlx to connect to db instance

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,9 @@
 //! tests/health_check.rs
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
 use serde::Deserialize;
-use zero2prod::app;
+use zero2prod::startup::app;
 
 // Define a test struct that matches the HealthResponse struct
 #[derive(Debug, Deserialize, PartialEq)]
@@ -80,12 +82,19 @@ async fn complex_health_check_works() {
 async fn subscribe_returns_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    // The  `Connection` trait must be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("http://{}/subscriptions", app_address))
+        .post(&format!("http://{}/subscriptions", &app_address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
